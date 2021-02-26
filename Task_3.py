f3140b5e613b3b556ff6c1b7efa6a3c774f8f914
mardrake <mardrake@mail.ru>
2021-02-26 17:32:53 +0300

    task_3 done

--- Task_3.py
+++ Task_3.py
@@ -1,1 +1,23 @@
-import numpy as np
+import math
+
+# Ввести координаты синего вектора
+x1 = float(input('x1 = '))
+y1 = float(input('y1 = '))
+z1 = float(input('z1 = '))
+
+lvec1 = math.sqrt(x1 * x1 + y1 * y1 + z1 * z1)
+
+print('Length blue = ', lvec1)
+
+# Ввести координаты красного вектора
+x2 = float(input('x2 = '))
+y2 = float(input('y2 = '))
+z2 = float(input('z2 = '))
+
+lvec2 = math.sqrt(x2 * x2 + y2 * y2 + z2 * z2)
+
+print('Length red = ', lvec2)
+
+lvec3 = math.sqrt(lvec2 * lvec2 - lvec1 * lvec1)
+
+print('Length green = ', lvec3)
